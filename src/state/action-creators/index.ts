b403e70c4d87f700f1a35d5a13d6d61aefb0d2ba
adisekar <adithya.kiran.sekar@gmail.com>
2021-09-28 00:17:41 -0400

    Finished app

---
+++ src/state/action-creators/index.ts
@@ -0,0 +1,32 @@
+import axios from "axios";
+import { Action } from "../actions";
+import { ActionType } from "../action-types";
+import { Dispatch } from "redux";
+
+export const searchRepositories = (term: string) => {
+    return async (dispatch: Dispatch<Action>) => {
+        dispatch({
+            type: ActionType.SEARCH_REPOSITORIES
+        });
+
+        try {
+            const { data } = await axios.get('http://registry.npmjs.com/-/v1/search', {
+                params: {
+                    text: term
+                }
+            });
+            const names = data.objects.map((result: any) => {
+                return result.package.name;
+            })
+            dispatch({
+                type: ActionType.SEARCH_REPOSITORIES_SUCCESS,
+                payload: names
+            });
+        } catch (err: any) {
+            dispatch({
+                type: ActionType.SEARCH_REPOSITORIES_ERROR,
+                payload: err.message
+            });
+        }
+    };
+}
